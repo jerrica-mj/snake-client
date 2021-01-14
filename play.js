1bae4821a292ee742694baa2fb2a3602191b8325
JJ <jerrica.mj@gmail.com>
2021-01-14 04:15:14 +0000

    Refactored and modularized play.js by removing connect() to the client.js file and using module export/require. -jj

--- play.js
+++ play.js
@@ -1,24 +1,6 @@
 // Main file for launching the snake game client
 
-
-const net = require('net');
-
-/**
- * Establishes connection with the game server
- */
-const connect = function() {
-  const conn = net.createConnection({
-    host: 'localhost',
-    port: 50541
-  });
-  // interpret incoming data as text
-  conn.setEncoding('utf8');
-  // handle incoming data from the server and log for the player
-  conn.on('data', (data) => {
-    console.log('Server: ', data);
-  });
-  return conn;
-}
+const {connect} = require('./client');
 
 console.log('Connecting ...');
 connect();
