31b53fc35e70451af41673f6c01e113751b30aad
JJ <jerrica.mj@gmail.com>
2021-01-14 06:01:16 +0000

    Added stdin (standart input) object to listen for user input via keyboard. Set up handleUserInput() to enable 'ctrl + c' to exit the program. -jj

--- play.js
+++ play.js
@@ -4,3 +4,28 @@ const {connect} = require('./client');
 
 console.log('Connecting ...');
 connect();
+
+/**
+ * Setup User Interface
+ * Specifically to handle user input via stdin
+ **/
+
+// Function to handle Ctrl + C user input
+const handleUserInput = (keyPress) => {
+  // enable ctrl + c to terminate the program
+  // \u0003 maps to ctrl+c input
+  if (keyPress === '\u0003') {
+    process.exit();
+  }
+};
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data', handleUserInput);
+  return stdin;
+};
+
+setupInput();
